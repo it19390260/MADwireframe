b3302d9e4f7e54103a40fe195845082b9e8a6e32
Pasindu <sandungwp@gmail.com>
2020-08-15 01:29:37 +0530

    new filesss

---
+++ app/src/main/java/com/example/mad1/LocationDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mad1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LocationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_details);
+    }
+}
