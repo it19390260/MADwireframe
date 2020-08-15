fe78ef69909bb457cbcc9e596b79c03b5d970916
Pasindu <sandungwp@gmail.com>
2020-08-15 18:37:25 +0530

    permision_like

---
+++ app/src/main/java/com/example/mad1/PermissionRequestActivity.java
@@ -0,0 +1,14 @@
+package com.example.mad1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PermissionRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_request);
+    }
+}
